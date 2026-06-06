f3859c42db0a7dcd38cf243651d8a34a20469a47
Paige Li <itspaige.nli@gmail.com>
2026-06-06 13:08:31 -0700

    Add signup page component shell

---
+++ client/src/components/SignupPage.jsx
@@ -0,0 +1,10 @@
+function SignupPage() {
+  return (
+    <section className="panel form-panel">
+      <p className="section-label">Account Access</p>
+      <h1>Sign Up</h1>
+    </section>
+  );
+}
+
+export default SignupPage;
