05ddd99f2635e2f09adf2a3844e27e8a8e8306f7
Paige Li <itspaige.nli@gmail.com>
2026-06-06 14:03:19 -0700

    Add login form state

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -1,4 +1,19 @@
+import { useState } from "react";
+
 function LoginPage() {
+  const [form, setForm] = useState({
+    identifier: "",
+    password: "",
+  });
+
+  function updateForm(event) {
+    const { name, value } = event.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  }
   return (
     <section className="panel form-panel">
       <p className="section-label">Account Access</p>
