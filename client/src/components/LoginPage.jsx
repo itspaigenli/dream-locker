5d144fbe6f05a74ad84c4f56658ab45c15a04438
Paige Li <itspaige.nli@gmail.com>
2026-06-06 13:57:19 -0700

    Add login page component shell

---
+++ client/src/components/LoginPage.jsx
@@ -0,0 +1,10 @@
+function LoginPage() {
+  return (
+    <section className="panel form-panel">
+      <p className="section-label">Account Access</p>
+      <h1>Login</h1>
+    </section>
+  );
+}
+
+export default LoginPage;
