fc7b75a5037e9a6fb11ffea909b20e5833b485fd
Paige Li <itspaige.nli@gmail.com>
2026-06-06 13:55:26 -0700

    Add signup navigation button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,6 +25,9 @@ function Header({ onPageChange, onArchiveClick }) {
         <button type="button" onClick={onArchiveClick}>
           Archive
         </button>
+        <button type="button" onClick={() => onPageChange("signup")}>
+          Sign Up
+        </button>
       </nav>
     </header>
   );
