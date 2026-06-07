714baa7c7834b4e59e5ec1415ed96d1a2f7bc05a
Paige Li <itspaige.nli@gmail.com>
2026-06-07 10:56:14 -0700

    Test 2 - Input boxes accept Text

--- client/src/components/LoginPage.test.jsx
+++ client/src/components/LoginPage.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import LoginPage from "./LoginPage.jsx";
+import userEvent from "@testing-library/user-event";
 
 /* describe("ComponentName", () => {
   it("what the test checks", () => {
@@ -28,6 +29,25 @@ describe("LoginPage", () => {
 });
 
 // Test 2: Input boxes accept text
+describe("LoginPage", () => {
+  it("lets the user type into the login form", async () => {
+    render(
+      <LoginPage
+        API_URL="http://localhost:3000/api"
+        onLoginSuccess={() => {}}
+      />,
+    );
+
+    const userInput = screen.getByLabelText("Username or Email");
+    const passwordInput = screen.getByLabelText("Password");
+
+    await userEvent.type(userInput, "admin@example.com");
+    await userEvent.type(passwordInput, "AdminPassword1!");
+
+    expect(userInput).toHaveValue("admin@example.com");
+    expect(passwordInput).toHaveValue("AdminPassword1!");
+  });
+});
 
 // Test 3: Form Submission works
 
