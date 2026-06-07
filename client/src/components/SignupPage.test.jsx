1af89bf25ef963ce02f2aeef57e381163f331a78
Paige Li <itspaige.nli@gmail.com>
2026-06-07 11:45:52 -0700

    Start signup page tests

---
+++ client/src/components/SignupPage.test.jsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, expect, it } from "vitest";
+import SignupPage from "./SignupPage.jsx";
+
+// Test 1: Render the form
+
+// Test 2: Form sends signup request
+
+// Test 3: Successful signup
+
+// Test 4: Unsuccessful signup
