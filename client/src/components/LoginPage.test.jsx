12c35652f5c4ece8f2b83d57c871ae52c3dc4463
Paige Li <itspaige.nli@gmail.com>
2026-06-07 10:52:50 -0700

    Test 1 - Render

--- client/src/components/LoginPage.test.jsx
+++ client/src/components/LoginPage.test.jsx
@@ -2,7 +2,30 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import LoginPage from "./LoginPage.jsx";
 
+/* describe("ComponentName", () => {
+  it("what the test checks", () => {
+    render(component)
+
+    expect(thing on page).toBeInTheDocument()
+  });
+}); */
+
 // Test 1: Render the form
+describe("LoginPage", () => {
+  it("shows the login form", () => {
+    render(
+      <LoginPage
+        API_URL="http://localhost:3000/api"
+        onLoginSuccess={() => {}}
+      />,
+    );
+
+    expect(screen.getByText("Login")).toBeInTheDocument();
+    expect(screen.getByLabelText("Username or Email")).toBeInTheDocument();
+    expect(screen.getByLabelText("Password")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Login" })).toBeInTheDocument();
+  });
+});
 
 // Test 2: Input boxes accept text
 
