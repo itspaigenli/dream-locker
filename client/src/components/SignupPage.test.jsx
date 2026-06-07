e0a13cf4b0668d967365904d8ddfcce445274fb6
Paige Li <itspaige.nli@gmail.com>
2026-06-07 11:48:37 -0700

    Test 1 - Rendering

--- client/src/components/SignupPage.test.jsx
+++ client/src/components/SignupPage.test.jsx
@@ -3,7 +3,32 @@ import userEvent from "@testing-library/user-event";
 import { describe, expect, it } from "vitest";
 import SignupPage from "./SignupPage.jsx";
 
+/* describe("SignupPage", () => {
+  it("shows the signup form", () => {
+    render(<SignupPage API_URL="http://localhost:3000/api" setPage={() => {}} />);
+
+    expect(screen.getByRole("heading", { name: "Sign Up" })).toBeInTheDocument();
+  });
+}); */
+
 // Test 1: Render the form
+describe("SignupPage", () => {
+  it("shows the signup form", () => {
+    render(
+      <SignupPage API_URL="http://localhost:3000/api" setPage={() => {}} />,
+    );
+
+    expect(
+      screen.getByRole("heading", { name: "Sign Up" }),
+    ).toBeInTheDocument();
+    expect(screen.getByLabelText("Username")).toBeInTheDocument();
+    expect(screen.getByLabelText("Email")).toBeInTheDocument();
+    expect(screen.getByLabelText("Password")).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Create Account" }),
+    ).toBeInTheDocument();
+  });
+});
 
 // Test 2: Form sends signup request
 
