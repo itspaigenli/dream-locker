4eff6e91b9865c66cc277be2d9c6eb7be3968a1c
Paige Li <itspaige.nli@gmail.com>
2026-06-07 12:07:17 -0700

    Add authenticated fetch tests

--- client/api/authentificatedFetch.test.js
+++
@@ -1,6 +0,0 @@
-import { describe, expect, it } from "vitest";
-import { authenticatedFetch } from "./authenticatedFetch.js";
-
-// Test 1: Sends token in Authorization header
-
-// Test 2: Throws error when request fails
