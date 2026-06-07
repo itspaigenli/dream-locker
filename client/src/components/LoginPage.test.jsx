31ebbebcbe21f02efe84e2e2e38e230facd4afe4
Paige Li <itspaige.nli@gmail.com>
2026-06-07 10:50:23 -0700

    Test File: Login Page

---
+++ client/src/components/LoginPage.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import LoginPage from "./LoginPage.jsx";
+
+// Test 1: Render the form
+
+// Test 2: Input boxes accept text
+
+// Test 3: Form Submission works
+
+// Test 4: Successful Login
+
+// Test 5: Unsuccessful Login
